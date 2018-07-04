5744727d1b5f17cbed378dfb7c6680351d4ddf3f
natmark <natmark0918@gmail.com>
2018-07-05 06:09:30 +0900

    :sparkles: Add Colorful sample

--- OSCProcessingKitDemo/Sketches/Colorful/ColorfulView.swift
+++ OSCProcessingKitDemo/Sketches/Colorful/ColorfulView.swift
@@ -1,27 +1,23 @@
-//
-//  ColorfulView.swift
-//  OSCProcessingKitDemo
-//
-//  Created by AtsuyaSato on 2018/07/04.
-//  Copyright © 2018年 Atsuya Sato. All rights reserved.
-//
+// Colorful - ProcessingKit
 
-import Foundation
 import UIKit
 import ProcessingKit
 
-class ColorfulView : ProcessingView {
+class ColorfulView: ProcessingView {
     func setup() {
         background(0, 0, 0)
         noStroke()
     }
 
     func draw() {
-        print(fingerPressed)
         if fingerPressed {
             fill(UIColor(hue: random(255) / 255.0, saturation: 255 / 255.0, brightness: 255 / 255.0, alpha: 255 / 255.0))
             let size = CGFloat(Int(random(20)) % 18)
             ellipse(touchX + random(20) - 10, touchY + random(20) - 10, size, size)
+
+            let x = touchX + random(20) - 10
+            let y = touchY + random(20) - 10
+            ellipse(x, y, size, size)
         }
     }
 
